b2e81b5cb9a40894dfc02a6cbd5a867afa9e458d
tyougii <evzh238@gmail.com>
2025-08-18 01:07:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from pyscript import display
+from js import document
 
-# 定义一个简单的字符串
-message = "Hello from tyou gii .main.py inside!"
+message = "Hello from main.py!"
+document.getElementById("output").innerText = message
 
-# 将字符串显示到HTML页面中id为"output"的元素里
-display(message, target="output")
-
-# 或者你可以打印到控制台，然后在浏览器中检查
-# print("This message is from main.py, check the console!")
