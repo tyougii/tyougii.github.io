5e68f6d99d30c2d3748d58c61248006bc1d1a285
tyougii <evzh238@gmail.com>
2025-08-18 00:42:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pyscript import display  # 导入pyscript的display函数
 
 # 绘制图形代码
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
 plt.plot(x, y)
-plt.show()
+
+# 获取当前的图形对象
+fig = plt.gcf()
+
+# 使用display函数将图形渲染到指定的HTML元素中
+display(fig, target="my-plot")
