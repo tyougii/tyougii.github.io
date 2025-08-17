0c260a448d38bab0cd9d708075477558e70db1db
tyougii <evzh238@gmail.com>
2025-08-18 00:51:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyscript import display
 
 # 定义一个简单的字符串
-message = "Hello from main.py!"
+message = "Hello from tyou gii .main.py inside!"
 
 # 将字符串显示到HTML页面中id为"output"的元素里
 display(message, target="output")
